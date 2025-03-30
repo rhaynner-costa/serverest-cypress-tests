cabbc920cb92c2da5b39eb193e1310fe48cb2a51
Rhaynner Costa <rhaynner@me.com>
2025-03-30 18:29:56 -0300

    organizacao dos testes e2e

--- cypress/e2e/navegaTelaHome.cy.js
+++ cypress/e2e/navegaTelaHome.cy.js
@@ -1,21 +1,30 @@
+
 const Selectors = require('../selectors/home');
-describe('TEste login', () => {
+const CONSTANTS = require('../helpers/constants');
+
+
+describe('Testes e2e do serveRest', () => {
+    const { emailValido, senha } = CONSTANTS;
 
     beforeEach(() => {
-        cy.login("alexa34564@qa.com", "teste123");
+        cy.login(emailValido, senha);
         cy.visit(Cypress.env('baseUrlE2E') + '/admin/home');
     });
 
-    it('Acessar a lista de usuarios', () => {
-        cy.validarTexto(Selectors.subTituloPagina, 'Este é seu sistema para administrar seu ecommerce.');
-        cy.clica(Selectors.botaoListarUsuario);
-        cy.validarTexto(Selectors.tituloPagina, 'Lista dos usuários');
-    });
+    context('Testes da navegacao pela tela home da aplicacao', () => {
 
-    it('Acessar a lista de produtos', () => {
-        cy.validarTexto(Selectors.subTituloPagina, 'Este é seu sistema para administrar seu ecommerce.');
-        cy.clica(Selectors.botaoListarProdutos);
-        cy.validarTexto(Selectors.tituloPagina, 'Lista dos Produtos');
+        it('Acessar a lista de usuarios', () => {
+            cy.validarTexto(Selectors.subTituloPagina, 'Este é seu sistema para administrar seu ecommerce.');
+            cy.clica(Selectors.botaoListarUsuario);
+            cy.validarTexto(Selectors.tituloPagina, 'Lista dos usuários');
+        });
+
+        it('Acessar a lista de produtos', () => {
+            cy.validarTexto(Selectors.subTituloPagina, 'Este é seu sistema para administrar seu ecommerce.');
+            cy.clica(Selectors.botaoListarProdutos);
+            cy.validarTexto(Selectors.tituloPagina, 'Lista dos Produtos');
+        });
+
     });
 
 });
